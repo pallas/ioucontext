6aa3bc20e44ede9e26eae1c3250277825bea406b
Derrick Pallas <derrick@pallas.us>
2024-01-01 13:03:17 -0800

    operations: add printf format attribute

--- operations.h
+++ operations.h
@@ -41,7 +41,7 @@ bool iou_poll_hup(reactor_t *, int fd, const struct timespec delta);
 bool iou_poll_in(reactor_t *, int fd, const struct timespec delta);
 bool iou_poll_out(reactor_t *, int fd, const struct timespec delta);
 ssize_t iou_pread(reactor_t *, int fildes, void *buf, size_t nbyte, off_t offset);
-int iou_printf(reactor_t *, int fd, const char *format, ...);
+int iou_printf(reactor_t *, int fd, const char *format, ...) __attribute__ ((format (printf, 3, 4)));
 ssize_t iou_pwrite(reactor_t *, int fildes, const void *buf, size_t nbyte, off_t offset);
 ssize_t iou_read(reactor_t *, int fildes, void *buf, size_t nbyte);
 ssize_t iou_recv(reactor_t *, int socket, void *buffer, size_t length, int flags);
