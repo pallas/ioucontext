2882a228e144727e9652b7069be4bcfff1f5e980
Derrick Lyndon Pallas <derrick@pallas.us>
2025-06-18 04:17:14 +0000

    mutex: rework with futex, break out recursive version
    This commit is ABI breaking; iou_mutex_t is now just an Atomic uint32_t and
    the lock is not recursive.  The recursive lock is instead implemented by
    iou_mootex_t, which wraps iou_mutex_t and uses `reactor_current` to track
    ownership.  References to the running fiber and the reactor's workqueue are
    gone, replaced by futex operations.

--- mutex.c
+++ mutex.c
@@ -1,63 +1,108 @@
 // SPDX-License-Identifier: MIT
 #include "mutex.h"
 
-#include "reactor-internal.h"
-#include "jump_queue.h"
-#include "todo_sigjmp.h"
+#include "macros.h"
+#include "operations.h"
+#include "reactor.h"
+#include "timespec.h"
 
 #include <assert.h>
 
-bool
-iou_mutex_taken(reactor_t * reactor, const iou_mutex_t * mutex) {
-    assert(!mutex->owner || mutex->depth);
-    return mutex->owner;
+void
+iou_mutex_build(reactor_t * reactor, iou_mutex_t * mutex) {
+    atomic_init(&mutex->value, 0);
 }
 
 bool
-iou_mutex_probe(reactor_t * reactor, iou_mutex_t * mutex) {
-    assert(!mutex->owner || mutex->depth);
-    fiber_t * whoami = reactor->current ?: (fiber_t*)reactor;
-
-    if (!mutex->owner)
-        mutex->owner = whoami;
-
-    return mutex->owner == whoami && ++mutex->depth;
+iou_mutex_knock(reactor_t * reactor, iou_mutex_t * mutex) {
+    iou_mutex_value_t shadow = 0;
+    return atomic_compare_exchange_weak_explicit(
+        &mutex->value, &shadow, 1,
+        memory_order_acquire, memory_order_relaxed
+        ) && 0 == shadow;
 }
 
 void
 iou_mutex_enter(reactor_t * reactor, iou_mutex_t * mutex) {
-    assert(!mutex->owner || mutex->depth);
-    fiber_t * whoami = reactor->current ?: (fiber_t*)reactor;
+    if (iou_mutex_knock(reactor, mutex))
+        return;
 
-    while (!iou_mutex_probe(reactor, mutex)) {
-        todo_sigjmp_t todo;
-        if (!sigsetjmp(*make_todo_sigjmp(&todo, reactor->current), false)) {
-            jump_queue_enqueue((jump_queue_t*)&mutex->waiters, &todo.jump);
-            reactor_enter_core(reactor);
-        }
-    }
-    assert(mutex->owner == whoami);
+    iou_yield(reactor);
+
+    if (!iou_mutex_knock(reactor, mutex))
+    while (0 != atomic_exchange_explicit(&mutex->value, -1, memory_order_acq_rel))
+        iou_futex_wait32(reactor, (uint32_t*)&mutex->value, -1, timespec_block);
+
+    assert(iou_mutex_taken(reactor, mutex));
 }
 
 bool
-iou_mutex_owner(reactor_t * reactor, const iou_mutex_t * mutex) {
-    assert(!mutex->owner || mutex->depth);
-    fiber_t * whoami = reactor->current ?: (fiber_t*)reactor;
-    return mutex->owner == whoami;
+iou_mutex_taken(reactor_t * reactor, const iou_mutex_t * mutex) {
+    return 0 != atomic_load_explicit(&mutex->value, memory_order_acquire);
 }
 
 void
 iou_mutex_leave(reactor_t * reactor, iou_mutex_t * mutex) {
-    assert(mutex->depth);
-    fiber_t * whoami = reactor->current ?: (fiber_t*)reactor;
-    assert(mutex->owner == whoami);
+    assert(iou_mutex_taken(reactor, mutex));
+    if (-1 == atomic_exchange_explicit(&mutex->value, 0, memory_order_acq_rel))
+        iou_futex_wake32_fast(reactor, (uint32_t*)&mutex->value, 1);
+}
 
-    if (--mutex->depth)
-        return;
 
-    mutex->owner = NULL;
-    if (!jump_queue_empty((jump_queue_t*)&mutex->waiters))
-        reactor_schedule(reactor, jump_queue_dequeue((jump_queue_t*)&mutex->waiters));
+void
+iou_mootex_build(reactor_t * reactor, iou_mootex_t * mootex) {
+    iou_mutex_build(reactor, &mootex->mutex);
+    atomic_init(&mootex->owner, 0);
+    mootex->depth = 0;
+}
+
+bool
+iou_mootex_knock(reactor_t * reactor, iou_mootex_t * mootex) {
+    uintptr_t whoami = reactor_current(reactor);
+    if (whoami == atomic_load_explicit(&mootex->owner, memory_order_acquire)) {
+        ++mootex->depth;
+        return true;
+    }
+
+    if (iou_mutex_knock(reactor, &mootex->mutex)) {
+        assert(0 == mootex->depth);
+        mootex->depth = 1;
+        assert(!atomic_load_explicit(&mootex->owner, memory_order_acquire));
+        atomic_store_explicit(&mootex->owner, whoami, memory_order_release);
+        return true;
+    }
+
+    return false;
+}
+
+void
+iou_mootex_enter(reactor_t * reactor, iou_mootex_t * mootex) {
+    uintptr_t whoami = reactor_current(reactor);
+    if (whoami == atomic_load_explicit(&mootex->owner, memory_order_acquire)) {
+        ++mootex->depth;
+    } else {
+        iou_mutex_enter(reactor, &mootex->mutex);
+        assert(0 == mootex->depth);
+        mootex->depth = 1;
+        assert(!atomic_load_explicit(&mootex->owner, memory_order_acquire));
+        atomic_store_explicit(&mootex->owner, whoami, memory_order_release);
+    }
+}
+
+bool
+iou_mootex_taken(reactor_t * reactor, const iou_mootex_t * mootex) {
+    return iou_mutex_taken(reactor, &mootex->mutex);
+}
+
+void
+iou_mootex_leave(reactor_t * reactor, iou_mootex_t * mootex) {
+    uintptr_t whoami = reactor_current(reactor);
+    if (UNLIKELY(whoami != atomic_load_explicit(&mootex->owner, memory_order_acquire)))
+        abort();
+    if (!--mootex->depth) {
+        atomic_store_explicit(&mootex->owner, 0, memory_order_release);
+        iou_mutex_leave(reactor, &mootex->mutex);
+    }
 }
 
 //
--- mutex.h
+++ mutex.h
@@ -2,30 +2,38 @@
 #ifndef IOUCONTEXT_MUTEX_H
 #define IOUCONTEXT_MUTEX_H
 
+#include <stdatomic.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef struct fiber_s fiber_t;
 typedef struct reactor_s reactor_t;
-typedef struct jump_chain_s jump_chain_t;
 
+typedef _Atomic uint32_t iou_mutex_value_t;
 typedef struct iou_mutex_s {
-    int depth;
-    fiber_t *owner;
-    struct /*jump_chain_s*/ {
-        jump_chain_t * head;
-        jump_chain_t ** tail;
-    } waiters;
+    iou_mutex_value_t value;
 } iou_mutex_t;
 
-bool iou_mutex_taken(reactor_t *, const iou_mutex_t *);
-bool iou_mutex_probe(reactor_t *, iou_mutex_t *);
+void iou_mutex_build(reactor_t *, iou_mutex_t *);
+bool iou_mutex_knock(reactor_t *, iou_mutex_t *);
 void iou_mutex_enter(reactor_t *, iou_mutex_t *);
-bool iou_mutex_owner(reactor_t *, const iou_mutex_t *);
+bool iou_mutex_taken(reactor_t *, const iou_mutex_t *);
 void iou_mutex_leave(reactor_t *, iou_mutex_t *);
+
+typedef struct iou_mootex_s {
+    iou_mutex_t mutex;
+    atomic_uintptr_t owner;
+    unsigned depth;
+} iou_mootex_t;
+
+void iou_mootex_build(reactor_t *, iou_mootex_t *);
+bool iou_mootex_knock(reactor_t *, iou_mootex_t *);
+void iou_mootex_enter(reactor_t *, iou_mootex_t *);
+bool iou_mootex_taken(reactor_t *, const iou_mootex_t *);
+void iou_mootex_leave(reactor_t *, iou_mootex_t *);
 
 #ifdef __cplusplus
 }
