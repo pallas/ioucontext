fb2f2d8509fd989acdedb3316897f2e6f44014b6
Derrick Lyndon Pallas <derrick@pallas.us>
2025-06-19 05:15:48 +0000

    mutex: relax atomic memory orders

--- mutex.c
+++ mutex.c
@@ -30,7 +30,7 @@ iou_mutex_enter(reactor_t * reactor, iou_mutex_t * mutex) {
     iou_yield(reactor);
 
     if (!iou_mutex_knock(reactor, mutex))
-    while (0 != atomic_exchange_explicit(&mutex->value, -1, memory_order_acq_rel))
+    while (0 != atomic_exchange_explicit(&mutex->value, -1, memory_order_acquire))
         iou_futex_wait32(reactor, (uint32_t*)&mutex->value, -1, timespec_block);
 
     assert(iou_mutex_taken(reactor, mutex));
@@ -38,13 +38,13 @@ iou_mutex_enter(reactor_t * reactor, iou_mutex_t * mutex) {
 
 bool
 iou_mutex_taken(reactor_t * reactor, const iou_mutex_t * mutex) {
-    return 0 != atomic_load_explicit(&mutex->value, memory_order_acquire);
+    return 0 != atomic_load_explicit(&mutex->value, memory_order_relaxed);
 }
 
 void
 iou_mutex_leave(reactor_t * reactor, iou_mutex_t * mutex) {
     assert(iou_mutex_taken(reactor, mutex));
-    if (-1 == atomic_exchange_explicit(&mutex->value, 0, memory_order_acq_rel))
+    if (-1 == atomic_exchange_explicit(&mutex->value, 0, memory_order_release))
         iou_futex_wake32_fast(reactor, (uint32_t*)&mutex->value, 1);
 }
 
@@ -59,7 +59,7 @@ iou_mootex_build(reactor_t * reactor, iou_mootex_t * mootex) {
 bool
 iou_mootex_knock(reactor_t * reactor, iou_mootex_t * mootex) {
     uintptr_t whoami = reactor_current(reactor);
-    if (whoami == atomic_load_explicit(&mootex->owner, memory_order_acquire)) {
+    if (whoami == atomic_load_explicit(&mootex->owner, memory_order_relaxed)) {
         ++mootex->depth;
         return true;
     }
@@ -67,8 +67,8 @@ iou_mootex_knock(reactor_t * reactor, iou_mootex_t * mootex) {
     if (iou_mutex_knock(reactor, &mootex->mutex)) {
         assert(0 == mootex->depth);
         mootex->depth = 1;
-        assert(!atomic_load_explicit(&mootex->owner, memory_order_acquire));
-        atomic_store_explicit(&mootex->owner, whoami, memory_order_release);
+        assert(!atomic_load_explicit(&mootex->owner, memory_order_relaxed));
+        atomic_store_explicit(&mootex->owner, whoami, memory_order_relaxed);
         return true;
     }
 
@@ -78,14 +78,14 @@ iou_mootex_knock(reactor_t * reactor, iou_mootex_t * mootex) {
 void
 iou_mootex_enter(reactor_t * reactor, iou_mootex_t * mootex) {
     uintptr_t whoami = reactor_current(reactor);
-    if (whoami == atomic_load_explicit(&mootex->owner, memory_order_acquire)) {
+    if (whoami == atomic_load_explicit(&mootex->owner, memory_order_relaxed)) {
         ++mootex->depth;
     } else {
         iou_mutex_enter(reactor, &mootex->mutex);
         assert(0 == mootex->depth);
         mootex->depth = 1;
-        assert(!atomic_load_explicit(&mootex->owner, memory_order_acquire));
-        atomic_store_explicit(&mootex->owner, whoami, memory_order_release);
+        assert(!atomic_load_explicit(&mootex->owner, memory_order_relaxed));
+        atomic_store_explicit(&mootex->owner, whoami, memory_order_relaxed);
     }
 }
 
@@ -97,10 +97,10 @@ iou_mootex_taken(reactor_t * reactor, const iou_mootex_t * mootex) {
 void
 iou_mootex_leave(reactor_t * reactor, iou_mootex_t * mootex) {
     uintptr_t whoami = reactor_current(reactor);
-    if (UNLIKELY(whoami != atomic_load_explicit(&mootex->owner, memory_order_acquire)))
+    if (UNLIKELY(whoami != atomic_load_explicit(&mootex->owner, memory_order_relaxed)))
         abort();
     if (!--mootex->depth) {
-        atomic_store_explicit(&mootex->owner, 0, memory_order_release);
+        atomic_store_explicit(&mootex->owner, 0, memory_order_relaxed);
         iou_mutex_leave(reactor, &mootex->mutex);
     }
 }
