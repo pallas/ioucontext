aa2c7c6267fc331dcd9531b9895c6daecddf7865
Derrick Lyndon Pallas <derrick@pallas.us>
2025-08-04 04:31:17 +0000

    eventcount: futex optimization
    Use the least significant bit of the ticket to indicate whether there are
    any waiters.  If the bit us unset on wait, attempt to set it; on failure,
    the ticket is already stale.  If successful, futex wait as usual.  When
    waking, unset the bit and only futex wake if it had been set.

--- eventcount.c
+++ eventcount.c
@@ -15,21 +15,29 @@ iou_eventcount_ticket(reactor_t * reactor, const iou_eventcount_t * eventcount) 
 }
 
 iou_eventcount_ticket_t
-iou_eventcount_wait(reactor_t * reactor, iou_eventcount_t * eventcount, const iou_eventcount_ticket_t ticket) {
-    while (ticket == atomic_load_explicit(&eventcount->ticket, memory_order_relaxed))
-        iou_futex_wait32(reactor, (uint32_t*)&eventcount->ticket, ticket, timespec_block);
+iou_eventcount_wait(reactor_t * reactor, iou_eventcount_t * eventcount, const iou_eventcount_ticket_t want) {
+    iou_eventcount_ticket_t have = atomic_load_explicit(&eventcount->ticket, memory_order_relaxed);
+    if (!(want & 1) && have == want && !atomic_compare_exchange_weak_explicit(
+        &eventcount->ticket, &have, have | 1,
+        memory_order_relaxed, memory_order_relaxed
+        )) {
+        /* stale ticket */
+    } else if ((want | 1) == (have | 1)) do {
+        iou_futex_wait32(reactor, (uint32_t*)&eventcount->ticket, want | 1, timespec_block);
+    } while ((want | 1) == atomic_load_explicit(&eventcount->ticket, memory_order_relaxed));
+
     return atomic_load_explicit(&eventcount->ticket, memory_order_acquire);
 }
 
 void
 iou_eventcount_wake(reactor_t * reactor, iou_eventcount_t * eventcount, int n) {
     iou_eventcount_ticket_t ticket = atomic_load_explicit(&eventcount->ticket, memory_order_relaxed);
-    do {
-    } while (!atomic_compare_exchange_weak_explicit(
-        &eventcount->ticket, &ticket, ticket + 1,
+    while (!atomic_compare_exchange_weak_explicit(
+        &eventcount->ticket, &ticket, (ticket & ~1) + 2,
         memory_order_release, memory_order_relaxed
-        ));
-    iou_futex_wake32_fast(reactor, (uint32_t*)&eventcount->ticket, n);
+        ) && (ticket & 1)) { }
+    if (ticket & 1)
+        iou_futex_wake32_fast(reactor, (uint32_t*)&eventcount->ticket, n);
 }
 
 void
